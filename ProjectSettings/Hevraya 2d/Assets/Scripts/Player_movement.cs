ebc9ef963be31a12a5df9d48a069531131612842
jonyplayz <jonathan.goren@gmail.com>
2021-04-10 22:32:55 +0300

    need a fix for switching animations
    added front walking animatiopon but switching isnt working right yeet fix

--- ProjectSettings/Hevraya 2d/Assets/Scripts/Player_movement.cs
+++ ProjectSettings/Hevraya 2d/Assets/Scripts/Player_movement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player_movement : MonoBehaviour
 {
+    public Animation walkingUPanim;
+      
     public string Up;
     public string Down;
     public string Left;
@@ -11,6 +13,12 @@ public class Player_movement : MonoBehaviour
     public float Speed;
     private Rigidbody2D rb;
     private Vector2 Movement;
+
+     private Vector3 mouse_pos;
+    public Transform target; //Assign to the object you want to rotate
+    private Vector3 object_pos;
+    private float angle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +28,23 @@ public class Player_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Animation an = GetComponent<Animation>();
+        mouse_pos = Input.mousePosition;
+        mouse_pos.z = 5.23f; //The distance between the camera and object
+        object_pos = Camera.main.WorldToScreenPoint(target.position);
+        mouse_pos.x = mouse_pos.x - object_pos.x;
+        mouse_pos.y = mouse_pos.y - object_pos.y;
+        angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+        //transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+        if(angle <= 180 && angle >=0){
+            
+            GetComponent<Animation>().Play("walking up tho");
+        }
+        else{
+            an.Play("WALKING");
+        }
+
         
       if (Input.GetKey(Up))
       {Movement += new Vector2(0f,Speed);}
