d14507bc4906d6184122f7a928a1158979a80b29
Maayooky <zilber.maayan@gmail.com>
2021-04-23 12:08:53 +0300

    Upload3
    Changed bullet/projectile mechanichs:
    1. bullet is now fired using colliders, instead of raycasts
    2. bullet has fire cooldown(curruntly turned off)
    3.bullet collides with boxes and gets destroyed on collision

--- ProjectSettings/Hevraya 2d/Assets/Scripts/Bullet.cs
+++ ProjectSettings/Hevraya 2d/Assets/Scripts/Bullet.cs
@@ -5,14 +5,42 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     // Start is called before the first frame update
+   public Rigidbody2D rb;
+   public Vector2 fireDir;
+   public  float fireRot;
+   public bool shoot = false;
     void Start()
     {
-       StartCoroutine(ExampleCoroutine());
+       //StartCoroutine(ExampleCoroutinetype1());
+       StartCoroutine(ExampleCoroutinetype2());
     }
-    IEnumerator ExampleCoroutine()
+    void Update()
     {
-    
+     if(shoot){
+        rb.velocity =fireDir;
+        rb.rotation = fireRot;
+     }   
+     else{
+
+      rb.velocity = new Vector2(0f,0f);
+     }
+    }
+
+    void OnCollisionEnter2D(Collision2D colision){
+        Destroy(gameObject);
+    }
+    IEnumerator ExampleCoroutinetype1()
+    {
+     
+        rb.velocity =fireDir;
+        yield return new WaitForSeconds(3);
+        Destroy(gameObject);
+    }
+    IEnumerator ExampleCoroutinetype2()
+    {
+     
         yield return new WaitForSeconds(5);
         Destroy(gameObject);
     }
+   
 }
--- ProjectSettings/Hevraya 2d/Assets/Scripts/RangedAttack.cs
+++ ProjectSettings/Hevraya 2d/Assets/Scripts/RangedAttack.cs
@@ -4,34 +4,73 @@ using UnityEngine;
 
 public class RangedAttack : MonoBehaviour
 {
-     private Transform firepoint;
-    public float bulletspeed =20f;
+    public Transform firepoint;
     public GameObject bulletPrefab;
     public string shootkey;
     private Vector3 mouse_pos;
     private Vector3 object_pos;
     public Vector2 shootdir;
     public Camera cam;
-    void Start() 
-    {
-        firepoint = gameObject.transform;
-    }
+    public LineRenderer lineRenderer;
+    public float bullet_cooldown;
+    bool bulletshot = false;
+   
     
     void Update()
     {
+    
         mouse_pos = cam.ScreenToWorldPoint(Input.mousePosition);
         shootdir = new Vector2(mouse_pos.x-firepoint.position.x,mouse_pos.y-firepoint.position.y);
 
-        if (Input.GetKeyDown(shootkey))
+        if (Input.GetKeyDown(shootkey)&& !bulletshot)
         {
-            RangeAttack();
+            //StartCoroutine( RangeAttacktype1());
+            RangedAttackType2();
+            
+           //StartCoroutine(Wait());
+           
         }
     }
 
-    void RangeAttack()
+    IEnumerator Wait(){
+        yield return new WaitForSeconds(bullet_cooldown);
+        bulletshot= false;
+    }
+
+    IEnumerator RangeAttacktype1()
     {
-        GameObject bullet = Instantiate(bulletPrefab,firepoint.position, firepoint.rotation);
-        Rigidbody2D rb2d = bullet.GetComponent<Rigidbody2D>();
-        rb2d.velocity= shootdir * bulletspeed;
+        bulletshot= true;
+      RaycastHit2D hitInfo =  Physics2D.Raycast(firepoint.position,shootdir);
+      Instantiate(bulletPrefab,firepoint.position,firepoint.rotation);
+      Bullet bullet = bulletPrefab.GetComponent<Bullet>();
+      
+      if(hitInfo)
+      {
+          bullet.fireDir = new Vector2(-firepoint.position.x,-firepoint.position.y) + hitInfo.point;
+          lineRenderer.SetPosition(0, firepoint.position);
+          lineRenderer.SetPosition(1, hitInfo.point);
+      }
+     else{
+         bullet.fireDir = shootdir;
+         lineRenderer.SetPosition(0, firepoint.position);
+         lineRenderer.SetPosition(1, new Vector2(firepoint.position.x, firepoint.position.y)+shootdir*100);
+     }
+     lineRenderer.enabled = true;
+     yield return new WaitForSeconds(0.2f);
+     
+
+     lineRenderer.enabled = false;
+     
+    }
+    void RangedAttackType2()
+    {
+        //bulletshot= true;
+       GameObject Bulletobject = Instantiate(bulletPrefab,firepoint.position,firepoint.rotation);
+      Bullet bullet = Bulletobject.GetComponent<Bullet>();
+      Rigidbody2D bulletRb = Bulletobject.GetComponent<Rigidbody2D>();
+      float angle = Mathf.Atan2(shootdir.x,shootdir.y) ;
+      bullet.fireDir = new Vector2(Mathf.Sin(angle),Mathf.Cos(angle))*100f;
+      bullet.fireRot = angle* Mathf.Rad2Deg;
+      bullet.shoot = true;
     }
 }
