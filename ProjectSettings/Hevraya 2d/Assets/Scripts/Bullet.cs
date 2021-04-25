63ef99ea549f6cb8a300823f58e654617a175a4c
Maayooky <zilber.maayan@gmail.com>
2021-04-25 12:52:21 +0300

    Revert "Upload3"
    This reverts commit d14507bc4906d6184122f7a928a1158979a80b29.

--- ProjectSettings/Hevraya 2d/Assets/Scripts/Bullet.cs
+++ ProjectSettings/Hevraya 2d/Assets/Scripts/Bullet.cs
@@ -5,42 +5,14 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     // Start is called before the first frame update
-   public Rigidbody2D rb;
-   public Vector2 fireDir;
-   public  float fireRot;
-   public bool shoot = false;
     void Start()
     {
-       //StartCoroutine(ExampleCoroutinetype1());
-       StartCoroutine(ExampleCoroutinetype2());
+       StartCoroutine(ExampleCoroutine());
     }
-    void Update()
+    IEnumerator ExampleCoroutine()
     {
-     if(shoot){
-        rb.velocity =fireDir;
-        rb.rotation = fireRot;
-     }   
-     else{
-
-      rb.velocity = new Vector2(0f,0f);
-     }
-    }
-
-    void OnCollisionEnter2D(Collision2D colision){
-        Destroy(gameObject);
-    }
-    IEnumerator ExampleCoroutinetype1()
-    {
-     
-        rb.velocity =fireDir;
-        yield return new WaitForSeconds(3);
-        Destroy(gameObject);
-    }
-    IEnumerator ExampleCoroutinetype2()
-    {
-     
+    
         yield return new WaitForSeconds(5);
         Destroy(gameObject);
     }
-   
 }
